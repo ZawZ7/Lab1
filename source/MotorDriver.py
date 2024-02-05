f088621085c858f64eef3ea152523cca426a46c1
seamuswr <108034107+seamuswr@users.noreply.github.com>
2024-02-05 14:52:37 -0800

    Added filtering of values outside of -100 to 100 acceptable threshold for duty cycle

--- source/MotorDriver.py
+++ source/MotorDriver.py
@@ -1,6 +1,7 @@
 from pyb import Pin, Timer
 import time
 
+
 class MotorDriver:
     # Constructor to initialize motor control pins and PWM timer
     def __init__(self, en_pin, IN1A_pin, IN2A_pin, PWM_tim):
@@ -12,18 +13,18 @@ class MotorDriver:
         @param IN2A_pin: Pin connected to IN2A of the motor driver
         @param timer: Timer used for PWM control
         """
-        print ("Creating a motor driver")
+        print("Creating a motor driver")
         self.EN = pyb.Pin(en_pin, pyb.Pin.OUT_OD, pyb.Pin.PULL_UP)
         self.IN1A = Pin(IN1A_pin, Pin.OUT_PP)
         self.IN2A = Pin(IN2A_pin, Pin.OUT_PP)
-        
+
         self.tim = PWM_tim
-        
+
         self.PWM1 = self.tim.channel(1, mode=Timer.PWM, pin=self.IN1A)  # Initialize PWM channel 1
         self.PWM2 = self.tim.channel(2, mode=Timer.PWM, pin=self.IN2A)  # Initialize PWM channel 2
-        
+
         self.EN.value(0)
-        
+
     # Method to set duty cycle for motor control
     def set_duty_cycle(self, duty):
         """
@@ -32,27 +33,42 @@ class MotorDriver:
         cause torque in one direction, negative values
         in the opposite direction.
         @param duty: A signed integer holding the duty
-               cycle of the voltage sent to the motor 
+               cycle of the voltage sent to the motor
         """
-        print (f"Setting Duty Cycle to {duty}")
-        
+        print(f"Setting Duty Cycle to {duty}")
+
         if duty > 0:
-            self.PWM1.pulse_width_percent(duty)  # Set duty cycle for forward motion
+            if duty > 100:
+                self.PWM1.pulse_width_percent(100)
+            else:
+                self.PWM1.pulse_width_percent(duty)  # Set duty cycle for forward motion
             self.PWM2.pulse_width_percent(0)
         elif duty < 0:
+            if duty < -100:
+                print('here')
+                self.PWM2.pulse_width_percent(100)
+            else:
+                self.PWM2.pulse_width_percent((-1) * duty)  # Set duty cycle for backward motion
             self.PWM1.pulse_width_percent(0)
-            self.PWM2.pulse_width_percent((-1)*duty)  # Set duty cycle for backward motion
         else:
             self.PWM1.pulse_width_percent(0)
             self.PWM2.pulse_width_percent(0)
         self.EN.value(1)
+
+
 #
 if __name__ == '__main__':
-            #testing the code
-            
-    moe = MotorDriver(pyb.Pin.board.PA10, pyb.Pin.board.PB4, pyb.Pin.board.PB5, pyb.Timer(3, freq = 20000))
-    moe.set_duty_cycle (-42)
+    # testing the code
+
+    moe = MotorDriver(pyb.Pin.board.PA10, pyb.Pin.board.PB4, pyb.Pin.board.PB5, pyb.Timer(3, freq=20000))
+    moe.set_duty_cycle(-42)
     time.sleep(4)
-    moe.set_duty_cycle (73)
+    moe.set_duty_cycle(73)
+    time.sleep(4)
+    moe.set_duty_cycle(0)
+    time.sleep(4)
+    moe.set_duty_cycle(-150)
+    time.sleep(4)
+    moe.set_duty_cycle(150)
     time.sleep(4)
     moe.set_duty_cycle(0)
